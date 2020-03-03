460582a02775f06d3cc813b7533b084edceb4de4
jgmuir2021 <jgmuir2021@tamu.edu>
2020-03-02 21:12:44 -0600

    Add files via upload

--- FrontFrame.java
+++ FrontFrame.java
@@ -11,8 +11,10 @@ import java.awt.event.*;
 public class FrontFrame {
 	
 	public static void main(String args[]) {
-		dbSetupExample dbAccess = new dbSetupExample(); //variable to hold Joseph's login info
+		dbSetup dbAccess = new dbSetup(); //variable to hold Joseph's login info
 		Connection connect = null; //variable to track connection status
+		
+		SQLCommands x = new SQLCommands();
 		
 		try { //connect here
 			//Class.forName("org.postgresql.Driver"); //I have no idea what this does
@@ -31,7 +33,215 @@ public class FrontFrame {
 		
 		final JFrame frontFrame = new JFrame("Front Frame"); //Parameter is box title
 		
-		//
+		String[] years = {"2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"};
+		String[] outputType = {"Text Box", "CVS"}; //Text Box is 0, CSV is 1
+		
+		//Input Fields for Conference
+		JTextField inputConferenceName = new JTextField(15);
+		inputConferenceName.setText("Conference Name");
+		JComboBox<String> yearChoices = new JComboBox<String>(years);
+		JComboBox<String> outputChoices = new JComboBox<String>(outputType);
+		JButton getOneConference = new JButton();
+		getOneConference.addActionListener(new ActionListener() {
+			//for one conference
+			public void actionPerformed(ActionEvent e) {
+				//get data to db
+				String confName = inputConferenceName.getText();
+				confName = "'" + confName + "'";
+				int index = yearChoices.getSelectedIndex();
+				String yearChoice = years[index];
+				index = outputChoices.getSelectedIndex();
+				boolean outChoice = false; //text box
+				if(index == 1) { //CVS
+					outChoice = true;
+				}
+				//send to one conference function
+				String output = x.conferenceTeams(confName, yearChoice, outChoice);
+				if(outChoice == false) {
+					JOptionPane.showMessageDialog(null, output);
+				} else {
+					csvCreator c = new csvCreator();
+					String fileOutputName = confName + ".csv";
+					c.CSV(output, fileOutputName);
+				}
+			}
+		});
+		getOneConference.setText("Conference Teams");
+		JButton getAllConferences = new JButton(); //expecting the string "all"
+		getAllConferences.addActionListener(new ActionListener() {
+			//for all conferences
+			public void actionPerformed(ActionEvent e) {
+				//get data to db
+				int index = yearChoices.getSelectedIndex();
+				String yearChoice = years[index];
+				index = outputChoices.getSelectedIndex();
+				boolean outChoice = false; //text box
+				if(index == 1) { //CVS
+					outChoice = true;
+				}
+				System.out.println(yearChoice);
+				System.out.println(outChoice);
+				//send to all conference function
+				
+			}
+		});
+		getAllConferences.setText("All Conferences");
+		
+		//Conference Panel
+		JPanel conferencePane = new JPanel();
+		//playerPane.add(new JLabel("First Name:")); labels aren't necessary or consistent with current design
+		conferencePane.add(inputConferenceName);
+		conferencePane.add(Box.createHorizontalStrut(10));
+		conferencePane.add(yearChoices);
+		conferencePane.add(Box.createHorizontalStrut(10));
+		conferencePane.add(outputChoices);
+		conferencePane.add(Box.createHorizontalStrut(10));
+		conferencePane.add(getOneConference);
+		conferencePane.add(Box.createHorizontalStrut(10));
+		conferencePane.add(getAllConferences);
+		
+		//Input Fields for Games
+		JTextField homeTeamName = new JTextField(15);
+		homeTeamName.setText("Enter Home Team");
+		JTextField visitingTeamName = new JTextField(15);
+		visitingTeamName.setText("Enter Visiting Team");
+		JButton gameInfo = new JButton();
+		gameInfo.addActionListener(new ActionListener() {
+			//get game information
+			public void actionPerformed(ActionEvent e) {
+				String homeName = homeTeamName.getText();
+				homeName = "'" + homeName + "'";
+				String visitingName = visitingTeamName.getText();
+				visitingName = "'" + visitingName + "'";
+				int index = yearChoices.getSelectedIndex();
+				String yearChoice = years[index];
+				index = outputChoices.getSelectedIndex();
+				boolean outChoice = false; //text box
+				if(index == 1) { //CVS
+					outChoice = true;
+				}
+				//send to get game information SQL
+			}
+		});
+		gameInfo.setText("Get Game Information");
+		JButton homeInfo = new JButton();
+		homeInfo.addActionListener(new ActionListener() {
+			//get home team information
+			public void actionPerformed(ActionEvent e) {
+				String homeName = homeTeamName.getText();
+				homeName = "'" + homeName + "'";
+				int index = yearChoices.getSelectedIndex();
+				String yearChoice = years[index];
+				index = outputChoices.getSelectedIndex();
+				boolean outChoice = false; //text box
+				if(index == 1) { //CVS
+					outChoice = true;
+				}
+				//send to get home team information SQL
+			}
+		});
+		homeInfo.setText("Home Team Stats");
+		JButton visitingInfo = new JButton();
+		visitingInfo.addActionListener(new ActionListener() {
+			//get visiting team information
+			public void actionPerformed(ActionEvent e) {
+				String visitingName = visitingTeamName.getText();
+				visitingName = "'" + visitingName + "'";
+				int index = yearChoices.getSelectedIndex();
+				String yearChoice = years[index];
+				index = outputChoices.getSelectedIndex();
+				boolean outChoice = false; //text box
+				if(index == 1) { //CVS
+					outChoice = true;
+				}
+				//send to get visiting team information SQL
+			}
+		});
+		visitingInfo.setText("Visiting Team Stats");
+		
+		//Games Panel
+		JPanel gamePanel = new JPanel();
+		gamePanel.add(homeTeamName);
+		gamePanel.add(Box.createHorizontalStrut(10));
+		gamePanel.add(visitingTeamName);
+		gamePanel.add(Box.createHorizontalStrut(10));
+		gamePanel.add(yearChoices);
+		gamePanel.add(Box.createHorizontalStrut(10));
+		gamePanel.add(outputChoices);
+		gamePanel.add(Box.createHorizontalStrut(10));
+		gamePanel.add(gameInfo);
+		gamePanel.add(Box.createHorizontalStrut(10));
+		gamePanel.add(homeInfo);
+		gamePanel.add(Box.createHorizontalStrut(10));
+		gamePanel.add(visitingInfo);
+		
+		//Input Fields for Stadium
+		JTextField inputStadiumField = new JTextField(15);
+		inputStadiumField.setText("Stadium Name");
+		JButton stadiumInfo = new JButton();
+		stadiumInfo.addActionListener(new ActionListener() {
+			//get stadium information
+			public void actionPerformed(ActionEvent e) {
+				String stadiumName = inputStadiumField.getText();
+				stadiumName = "'" + stadiumName + "'";
+				int index = yearChoices.getSelectedIndex();
+				String yearChoice = years[index];
+				index = outputChoices.getSelectedIndex();
+				boolean outChoice = false; //text box
+				if(index == 1) { //CVS
+					outChoice = true;
+				}
+				//send to get stadium info SQL
+			}
+		});
+		stadiumInfo.setText("Stadium Info");
+		JButton gamesAtStadium = new JButton();
+		gamesAtStadium.addActionListener(new ActionListener() {
+			//get stadium game information
+			public void actionPerformed(ActionEvent e) {
+				String stadiumName = inputStadiumField.getText();
+				stadiumName = "'" + stadiumName + "'";
+				int index = yearChoices.getSelectedIndex();
+				String yearChoice = years[index];
+				index = outputChoices.getSelectedIndex();
+				boolean outChoice = false; //text box
+				if(index == 1) { //CVS
+					outChoice = true;
+				}
+				//send to get stadium info SQL
+			}
+		});
+		gamesAtStadium.setText("Games Played at Stadium");
+		JButton allStadiums = new JButton();
+		allStadiums.addActionListener(new ActionListener() {
+			//get all stadium information
+			public void actionPerformed(ActionEvent e) {
+				int index = yearChoices.getSelectedIndex();
+				String yearChoice = years[index];
+				index = outputChoices.getSelectedIndex();
+				boolean outChoice = false; //text box
+				if(index == 1) { //CVS
+					outChoice = true;
+				}
+				//send to get all stadium SQL
+			}
+		});
+		allStadiums.setText("Get All Stadiums & Info");
+		
+		//Stadium Panel
+		JPanel stadiumPanel = new JPanel();
+		stadiumPanel.add(inputStadiumField);
+		stadiumPanel.add(Box.createHorizontalStrut(10));
+		stadiumPanel.add(yearChoices);
+		stadiumPanel.add(Box.createHorizontalStrut(10));
+		stadiumPanel.add(outputChoices);
+		stadiumPanel.add(Box.createHorizontalStrut(10));
+		stadiumPanel.add(stadiumInfo);
+		stadiumPanel.add(Box.createHorizontalStrut(10));
+		stadiumPanel.add(gamesAtStadium);
+		stadiumPanel.add(Box.createHorizontalStrut(10));
+		stadiumPanel.add(allStadiums);
+		
 		class MyItemListener implements ItemListener { 
             public void itemStateChanged(ItemEvent ev) {
                 boolean selected = (ev.getStateChange() == ItemEvent.SELECTED);
@@ -47,14 +257,20 @@ public class FrontFrame {
                         messageType = JOptionPane.INFORMATION_MESSAGE;
                         message = "Selecting this radio should show team search options";
                     } else if (command.equals("CONFERENCE")) {
-                        messageType = JOptionPane.INFORMATION_MESSAGE;
-                        message = "Selecting this radio should show conference search options";
+                    	messageType = JOptionPane.showConfirmDialog(null, conferencePane, "Conference",
+                    			JOptionPane.DEFAULT_OPTION);
+                        //messageType = JOptionPane.INFORMATION_MESSAGE;
+                        //message = "Selecting this radio should show conference search options";
                     } else if (command.equals("STADIUM")) {
-                        messageType = JOptionPane.INFORMATION_MESSAGE;
-                        message = "Selecting this radio should show stadium search options";
+                    	messageType = JOptionPane.showConfirmDialog(null, stadiumPanel, "Stadium",
+                    			JOptionPane.DEFAULT_OPTION);
+                        //messageType = JOptionPane.INFORMATION_MESSAGE;
+                        //message = "Selecting this radio should show stadium search options";
                     } else if (command.equals("GAMES")) {
-                    	messageType = JOptionPane.INFORMATION_MESSAGE;
-                    	message = "Selecting this radio should show game search information";
+                    	messageType = JOptionPane.showConfirmDialog(null, gamePanel, "Games",
+                    			JOptionPane.DEFAULT_OPTION);
+                    	//messageType = JOptionPane.INFORMATION_MESSAGE;
+                    	//message = "Selecting this radio should show game search information";
                     }
                     // show message
                     JOptionPane.showMessageDialog(frontFrame,
@@ -106,10 +322,9 @@ public class FrontFrame {
         cont.add(r2);
         cont.add(r3);
         cont.add(r4);
+        cont.add(r5);
  
         frontFrame.setVisible(true);
-		
-		System.out.println("End of code so far");
 	}
 
 }
